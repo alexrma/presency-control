84d1689d4aa5d251c594c52304afbf7a391d7efe
arodriguez5 <1020279409a>
2019-07-08 18:40:58 +0200

    add values from select

--- public/js/test.js
+++ public/js/test.js
@@ -1,2 +1,28 @@
-console.log('Test');
-console.log('Test');
+window.onload = function() {
+
+    clock();  
+      function clock() {
+      var now = new Date();
+      var TwentyFourHour = now.getHours();
+      var hour = now.getHours();
+      var min = now.getMinutes();
+    
+      if (min < 10) {
+        min = "0" + min;
+      }
+      if (hour > 12) {
+        hour = hour - 12;
+      }    
+      if(hour==0){ 
+        hour=12;
+      }
+      if(TwentyFourHour < 12) {
+         mid = 'am';
+      }     
+    document.getElementById('now').value =     hour+':'+min;
+      setTimeout(clock, 1000);
+      console.log(hour+':'+min);
+      }
+    
+  }
+  
